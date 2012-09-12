714e4ebf349a2849e9aaf9e76d04688c979f9d5e
orumin <icc.pot.tyew272@gmail.com>
2012-09-12 16:35:23 +0900

    Hello,World for Sony PSP HOMEBREW

---
+++ Makefile
@@ -0,0 +1,19 @@
+TARGET = helloworld
+OBJS = main.o
+
+INCDIR =
+CFLAGS = -O2 -G0 -Wall
+CXXFLAGS = $(CFLAGS) -fno-exceptions -fno-rtti
+ASFLAGS = $(CFLAGS)
+
+LIBDIR =
+LDFLAGS =
+
+EXTRA_TARGETS = EBOOT.PBP
+PSP_EBOOT_TITLE = Hello World
+
+BUILD_PRX = 1
+ENCRYPT = 1
+
+PSPSDK=$(shell psp-config --pspsdk-path)
+include $(PSPSDK)/lib/build.mak
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspctrl.h>
+#include <stdlib.h>
+#include <pspsdk.h>
+#include <psputility.h>
+
+PSP_MODULE_INFO("HelloWorld", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
+
+#define printf	pspDebugScreenPrintf
+
+int exit_callback(int arg1, int arg2, void *common)
+{
+  sceKernelExitGame();
+  return 0;
+}
+
+int CallbackThread(SceSize args, void *argp)
+{
+  int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+  sceKernelRegisterExitCallback(cbid);
+  sceKernelSleepThreadCB();
+  return 0;
+}
+
+int SetupCallbacks(void)
+{
+  int thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
+  if (thid >= 0)
+	sceKernelStartThread(thid, 0, 0);
+  return thid;
+}
+
+int main(void)
+{
+  pspDebugScreenInit();
+  printf("Hello World...\n");
+  sceKernelSleepThread();
+  return 0;
+}
